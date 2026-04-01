f3a863908729dd5a7c54ff676a53df9382fb23ae
LeidyMaingualDev <vanesamaingual2008@gmail.com>
2026-04-01 15:37:43 -0500

    feat: implement jwt service

---
+++ src/main/java/com/beutyglam/service/JwtService.java
@@ -0,0 +1,57 @@
+package com.beutyglam.service;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.Map;
+
+
+@Service
+public class JwtService {
+
+    @Value("${security.jwt.secret-key}")
+    private String secretKey;
+
+    @Value("${security.jwt.token-expiration}")
+    private Long expiration;
+
+    private SecretKey getKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generateToken(com.beutyglam.entity.User user) {
+        return Jwts.builder()
+                .claims(Map.of(
+                        "userId", user.getId(),
+                        "role", user.getRole().getRoleName()
+                ))
+                .subject(user.getEmail())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getKey())
+                .compact();
+    }
+
+    public boolean isValid(String token) {
+        try {
+            Jwts.parser().verifyWith(getKey()).build().parseSignedClaims(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public Claims getClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+}
