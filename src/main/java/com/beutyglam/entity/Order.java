c04a3a72f7975e82854b0cfd0ced8b1d542302e9
LauraAlejandraG12 <alejandrag900800qgmail.com>
2026-04-03 21:39:58 -0500

    feat(orders): create Order and OrderDetails entities

---
+++ src/main/java/com/beutyglam/entity/Order.java
@@ -0,0 +1,32 @@
+package com.beutyglam.entity;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name="orderProduct")
+public class Order {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long orderId;
+    
+    @ManyToOne
+    @JoinColumn(name = "fk_user_id")
+    private User user;
+
+    @Column(name = "order_date")
+    private LocalDate ordeDate;
+
+    @Column(name = "total")
+    private Double total;
+
+    @OneToMany(mappedBy = "fk_orderProduct_id", cascade = CascadeType.ALL)
+    private List<OrderDetails> details;
+
+}
